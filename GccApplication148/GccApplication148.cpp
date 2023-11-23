1ced6cd4a40a885353a7f616e009b79e2d2e6e74
IvanEnginer <ivan-17@yandex.ru>
2023-11-23 21:14:43 +0300

    load

---
+++ GccApplication148/GccApplication148.cpp
@@ -0,0 +1,67 @@
+#define F_CPU 8000000
+#include <avr/io.h>
+//#include <stdio.h>
+#include <util/delay.h>
+
+#define ADC_VREF_TYPE 0x60
+
+// Read the 8 most significant bits
+// of the AD conversion result
+unsigned char read_adc(unsigned char adc_input)
+{
+	ADMUX=adc_input | (ADC_VREF_TYPE & 0xff);
+	// Delay needed for the stabilization of the ADC input voltage
+	_delay_us(10);
+	// Start the AD conversion
+	ADCSRA|=0x40;
+	// Wait for the AD conversion to complete
+	while ((ADCSRA & 0x10)==0);
+	ADCSRA|=0x10;
+	return ADCH;
+}
+
+void uart_putchar( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSRA & (1<<UDRE)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR = data;
+}
+
+
+int main(void)
+{
+	DDRA=0;
+	PORTA=0;
+	DDRD=0;
+	PORTD=0;
+	
+	// USART initialization
+	// Communication Parameters: 8 Data, 1 Stop, No Parity
+	// USART Receiver: Off
+	// USART Transmitter: On
+	// USART Mode: Asynchronous
+	// USART Baud Rate: 9600
+	UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+	UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+	UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+	UBRRH=0x00;
+	UBRRL=0x33;
+
+	// ADC initialization
+	// ADC Clock frequency: 125,000 kHz
+	// ADC Voltage Reference: AVCC pin
+	// Only the 8 most significant bits of
+	// the AD conversion result are used
+	ADMUX=ADC_VREF_TYPE & 0xff;
+	ADCSRA=0x86;
+
+	
+	while(1)
+	{
+		_delay_ms(100);
+		uart_putchar(read_adc(0));
+		
+	}
+}
